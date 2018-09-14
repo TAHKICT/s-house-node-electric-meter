aa496fe8aa0b4521b970e75e2e28baf826f596f0
TAHKICT <mykh.roma@gmail.com>
2018-09-14 23:12:00 +0300

    electric meter adding

--- src/main/java/com/shouse/node/electricMeter/ElectricMeterNode.java
+++ src/main/java/com/shouse/node/electricMeter/ElectricMeterNode.java
@@ -1,4 +1,31 @@
 package com.shouse.node.electricMeter;
 
-public class ElectricMeterNode {
+import shouse.core.api.Notifier;
+import shouse.core.communication.NodeCommunicator;
+import shouse.core.communication.Packet;
+import shouse.core.node.Node;
+import shouse.core.node.NodeInfo;
+import shouse.core.node.NodeLocation;
+import shouse.core.node.request.Request;
+import shouse.core.node.response.Response;
+
+import java.util.List;
+
+public class ElectricMeterNode extends Node {
+
+    public ElectricMeterNode(int id, NodeLocation nodeLocation, NodeCommunicator nodeCommunicator, List<Notifier> notifiers, String description) {
+        super(id, nodeLocation, nodeCommunicator, notifiers, description);
+    }
+
+    public NodeInfo getNodeInfo() {
+        return null;
+    }
+
+    public Response processRequest(Request request) {
+        return null;
+    }
+
+    public void processPacket(Packet packet) {
+
+    }
 }
